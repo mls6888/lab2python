a6f17a5a4a977e3d3220f687f6fd05751671e824
mls6888 <mls6888@psu.edu>
2020-09-09 19:56:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+#Liam Sullivan mls6888@psu.edu
+# Collaborator: Andrew Ou ajo5499@psu.edu
+# Collaborator: Marvin Jakobs mjk5388@psu.edu
+# Collaborator: Fletcher Henneman fdh5031@psu.edu 
+# Collaborator: Brian Chetle bjc5969@psu.edu
+#  Breakout11
+def getLetterGrade(grade):
+  return "A"
+if (grade >= 93.0) 
+    return "A";
+  elif grade >= 90.0: 
+    return "A-" 
+  elif grade >= 87.0: 
+    return "B+"
+  elif grade >= 83.0: 
+    return "B" 
+  elif grade >= 80.0: 
+    return "B-"
+  elif grade >= 77.0:
+    return "C+" 
+  elif grade >= 70.0: 
+    return "C"  
+  elif grade >= 60.0: 
+    return "D"  
+  else:  
+    return "F"
+   
+
+
+
+def run():
+
+    grade = float(input("Enter your CMPSC 131 grade: ")) 
+    print(f"Your letter grade for CMPSC is {getLetterGrade(letter)}.")
+    
+if __name__ == "__main__":
+   run()
