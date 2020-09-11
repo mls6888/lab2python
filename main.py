a6ab77f00537977c588bbc574d3033bc00f7dccb
mls6888 <mls6888@psu.edu>
2020-09-11 17:52:12 +0000

    3

--- main.py
+++ main.py
@@ -5,23 +5,23 @@
 # Collaborator: Brian Chetle bjc5969@psu.edu
 # Section: 11r
 # Breakout : 11
-def getLetterGrade(grade):
-  if grade >= 93.0: 
-    return "A";
-  elif grade >= 90.0: 
-    return "A-" 
-  elif grade >= 87.0: 
-    return "B+"
-  elif grade >= 83.0: 
-    return "B" 
-  elif grade >= 80.0: 
-    return "B-"
-  elif grade >= 77.0:
-    return "C+" 
-  elif grade >= 70.0: 
-    return "C"  
-  elif grade >= 60.0: 
-    return "D"  
+def getGradePoint(grade):
+  if grade == "A": 
+    return 4.0
+  elif grade == "A-": 
+    return 3.67 
+  elif grade == "B+": 
+    return 3.33
+  elif grade == "B": 
+    return 3.0 
+  elif grade == "B-": 
+    return 2.67
+  elif grade == "C+":
+    return 2.33 
+  elif grade == "C": 
+    return 2.0  
+  elif grade == "D": 
+    return 1.0  
   else:  
     return "F"
    
@@ -29,9 +29,25 @@ def getLetterGrade(grade):
 
 
 def run():
+  gradepoint1 = getGradePoint(input("Enter your course 1 letter grade: ")) 
+  print(f"Grade point for course 1 is:",gradepoint1)
+  credit1 = input("Enter your course 1 credit: ")
+  credit1 = int(credit1)
+  gradepoint2 = getGradePoint(input("Enter your course 2 letter grade: "))
+  print(f"Grade point for course 2 is:",gradepoint2)
+  credit2 = input("Enter your course 2 credit: ")
+  credit2 = int(credit2)
+  gradepoint3 = getGradePoint(input("Enter your course 3 letter grade: ")) 
+  print(f"Grade point for course 3 is:",gradepoint3)
+  credit3 = input("Enter your course 3 credit: ")
+  credit3 = int(credit3)
+  GPA = (gradepoint1*credit1+gradepoint2*credit2+gradepoint3*credit3)/(credit1+credit2+credit3)
+  print("Your GPA is:",GPA)
 
-  grade = float(input("Enter your CMPSC 131 grade: ")) 
-  print(f"Your letter grade for CMPSC 131 is {getLetterGrade(grade)}.")
+  
+  
+  
+
     
 if __name__ == "__main__":
    run()
